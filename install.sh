e23e6777f3ee69ddfca206bdddf65409073fc0b9
DeAngelo-png <148593933+DeAngelo-png@users.noreply.github.com>
2025-06-30 01:01:16 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-cat <<"EOL" 
+cat <<"EOF" 
    ____         __       ____     __  _         
   /  _/__  ___ / /____ _/ / /__ _/ /_(_)__  ___ 
  _/ // _ \(_-</ __/ _ `/ / / _ `/ __/ / _ \/ _ \
 /___/_//_/___/\__/\_,_/_/_/\_,_/\__/_/\___/_//_/
                                                 
-EOL
+EOF
 echo "Detecting package manager..."
 
 if command -v pacman &> /dev/null; then
